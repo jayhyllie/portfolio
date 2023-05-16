09038a2d5acd056fbb91ddf4de6ecf523d746a20
Joakim Sundman <82515307+jayhyllie@users.noreply.github.com>
2023-05-16 11:05:15 +0200

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -34,7 +34,7 @@ function Home() {
                         </svg>
                     </a>
                 </section>
-                <a href="../src/assets/Joakim_Sundman.pdf" className={style.home__btn}>Download CV</a>
+                <a href="./Joakim_Sundman.pdf" className={style.home__btn}>Download CV</a>
             </section>
             <section className={style.home__right}>
                 <div className={style.profile}></div>
